ac1f4f9d183a8d1a183a243d53cf29d8897a701d
Fiona <fiona.valencia.chen@gmail.com>
2022-04-21 19:22:45 +0700

    cobain dari tutor web

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,48 @@
 import SwiftUI
 
-struct ContentView: View {
+struct Box {
+    var id: Int
+    let imageUrl : String
+}
+
+struct ContentView : View {
+
+    let boxes : [Box] = [
+    Box(id: 0, imageUrl: "back1"),
+    Box(id: 1, imageUrl: "back2"),
+    Box(id: 2, imageUrl: "back3"),
+    Box(id: 3, imageUrl: "back4"),
+    Box(id: 4, imageUrl: "back5"),
+    Box(id: 5, imageUrl: "back6"),
+    Box(id: 6, imageUrl: "back7"),
+    Box(id: 7, imageUrl: "back8")
+    ]
+    var body: some View {
+        NavigationView {
+            
+            ScrollView {
+                HStack {
+                    ForEach(boxes.identified(by: \.id)) {box in
+                        BoxView(box: box)
+                    }
+                }
+            }
+        }.padding(20)
+    }
+}
+
+
+
+struct BoxView: View {
+    
+    let box: Box
+    
     var body: some View {
         VStack {
-            
+            Image("\(box.imageUrl)")
+                .resizable()
+                .cornerRadius(12)
+                .frame(width: 80, height: 80)
         }
     }
 }
--- MyApp.swift
+++ MyApp.swift
@@ -19,38 +19,3 @@ struct MyApp: App {
                 }
             }
         }
-
-struct CardView : View {
-    
-    var data : Card
-    var body : some View {
-        
-        VStack(alignment: .leading, spacing: 0){
-            
-            Image(data.img)
-                .resizable()
-                .frame(width: UIScreen.main.bounds.width - 30, height: 460)
-        }
-    }
-}
-
-
-struct Card Identifiable {
-    
-    var id : Int
-    var img : String
-    var show : Bool
-}
-
-var data = [
-
-Card(id: 0, img: "back1", show: false),
-Card(id: 1, img: "back2", show: false),
-Card(id: 2, img: "back3", show: false),
-Card(id: 3, img: "back4", show: false),
-Card(id: 4, img: "back5", show: false),
-Card(id: 5, img: "back6", show: false),
-Card(id: 6, img: "back7", show: false),
-Card(id: 7, img: "back8", show: false),
-
-]
