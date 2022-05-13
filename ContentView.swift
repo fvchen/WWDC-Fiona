d4112b3571ccea14bbe5c196ac4f667d7a8d8e89
Fiona <fiona.valencia.chen@gmail.com>
2022-05-13 17:46:44 +0700

    udah dibenerin, copas dari yg udah bener - success

--- ContentView.swift
+++ ContentView.swift
@@ -3,120 +3,68 @@ import SwiftUI
 struct Box {
     var id: Int
     let imageUrl : String
+    var frontimageUrl : String
 }
 
 struct ContentView : View {
 
     let boxes : [Box] = [
-    Box(id: 0, imageUrl: "back1"),
-    Box(id: 1, imageUrl: "back2"),
-    Box(id: 2, imageUrl: "back3"),
-    Box(id: 3, imageUrl: "back4"),
-    Box(id: 4, imageUrl: "back5"),
-    Box(id: 5, imageUrl: "back6"),
-    Box(id: 6, imageUrl: "back7"),
-    Box(id: 7, imageUrl: "back8")
+    Box(id: 0, imageUrl: "backcard1", frontimageUrl: "frontcard1"),
+    Box(id: 1, imageUrl: "backcard2", frontimageUrl: "frontcard2"),
+    Box(id: 2, imageUrl: "backcard3", frontimageUrl: "frontcard3"),
+    Box(id: 3, imageUrl: "backcard4", frontimageUrl: "frontcard4"),
+    Box(id: 4, imageUrl: "backcard5", frontimageUrl: "frontcard5"),
+    Box(id: 5, imageUrl: "backcard6", frontimageUrl: "frontcard6"),
+    Box(id: 6, imageUrl: "backcard7", frontimageUrl: "frontcard7"),
+    Box(id: 7, imageUrl: "backcard8", frontimageUrl: "frontcard8")
     ]
-    
     var body: some View {
         NavigationView {
             ZStack{
                 GeometryReader {geo in
                                     ZStack{
-                                        Image("background")
+                                        Image("bg")
                                             .resizable()
                                             .scaledToFill()
                                             .edgesIgnoringSafeArea(.all)
                                             .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
                                             .opacity(1.0)
+                                   
+                                    
                                     
                                     }
                                 }
                 
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack {
-                        ForEach(0..<boxes.count){ i in BoxView(box: boxes[i], index: i)
-                                .frame(width: 300, height: 400)
-                                .position(x: 170, y: 250)
+                        ForEach(0..<boxes.count){I in BoxView(box: boxes[I])
+                                .frame(width: 265, height: 400)
+                                .position(x: 145, y: 235)
                         }
                     }
                 }
             }
+            
+            
         }.padding(20)
-        
-        struct BoxView: View {
-            }
-        
-        let box: Box
-        let index: Int
-        
-        let frontImages: [Box] = [
-            Box(id: 0, imageUrl: "task1"),
-            Box(id: 1, imageUrl: "task2"),
-            Box(id: 2, imageUrl: "task3"),
-            Box(id: 3, imageUrl: "task4"),
-            Box(id: 4, imageUrl: "task5"),
-            Box(id: 5, imageUrl: "task6"),
-            Box(id: 6, imageUrl: "task7"),
-            Box(id: 7, imageUrl: "task8")
-        ]
-        
-        
-        var body: some View {
-            VStack {
-                Image("\(box.imageUrl)")
-                    .resizable().onTapGesture {
-                        // masukin logic untuk ganti image ketika di tap
-                        Image("\(frontImages[0].imageUrl)").onTapGesture {
-                        
-                        }
-        
-                        @State var Box = Box(id: 0, imageUrl: "back1")
-                        @State var changeView = false
-                            
-                        //if index is tapped then display Image based on arraynya based on indexnya
-                        
-                        if index = is tapped {
-                            Image(frontImages)
-                        }
-                            
-                        
-                            var body: some View {
-                                changeView ? AnyView(theOtherView) : AnyView(theScrollView)
-                            }
-                            
-                            var theOtherView: some View {
-                                Image("back1").onTapGesture {
-                                id: 0
-                               self.changeView = false
-                                }
-                            }
-                            
-                            var theScrollView: some View {
-                                ScrollView() {
-                                    VStack {
-                                        
-                                        Image("task1")
-                                        id: 0
-                                            .resizable()
-                                            .scaledToFill()
-                                            .onTapGesture {
-                                                self.changeView = true
-                                            }
-                                            
-                                        }
-                                    }
-                                }
-                            }
-                         
-                        
-                       }
-                    
-                    }
-            }
-        
     }
+}
 
 
 
-
+struct BoxView: View {
+    
+    let box: Box
+    @State var turn = false
+    var tapGesture: some Gesture{
+        TapGesture().onEnded { _ in
+            self.turn.toggle()
+        }
+    }
+    var body: some View {
+        VStack {
+            Image("\(turn ? box.frontimageUrl : box.imageUrl)")
+                .resizable().gesture(tapGesture)
+        }
+    }
+}
